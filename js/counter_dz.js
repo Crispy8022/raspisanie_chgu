44eb7c57c80c3bb1300a87c5a33525328c527d3d
Selim <smutsaev2000@mail.ru>
2023-11-28 17:09:16 +0300

    28.11.23 17:09

--- js/counter_dz.js
+++ js/counter_dz.js
@@ -1,132 +1,52 @@
- // For Пн
- let counterPn = localStorage.getItem('counterPn') || 0;
+// Функция для обновления счетчика
+function updateCounter(day, counter) {
+    let count = localStorage.getItem(counter) || 0;
 
- function updateCounterPn(cellId) {
-     const table = document.querySelector(".dztable-pn");
-     const rows = table.getElementsByTagName('tr');
- 
-     counterPn = 0;
- 
-     for (let i = 1; i < rows.length; i++) {
-         const cell = rows[i].getElementsByTagName('td')[1];
-         if (cell.innerHTML.trim() !== "") {
-             counterPn++;
-         }
-     }
- 
-     document.querySelector(".spisok-pn").innerHTML = `Пн (${counterPn})`;
-     localStorage.setItem('counterPn', counterPn);
- }
- 
- document.querySelector(".spisok-pn").innerHTML = `Пн (${counterPn})`;
- 
- // For Вт
- let counterVt = localStorage.getItem('counterVt') || 0;
- 
- function updateCounterVt(cellId) {
-     const table = document.querySelector(".dztable-vt");
-     const rows = table.getElementsByTagName('tr');
- 
-     counterVt = 0;
- 
-     for (let i = 1; i < rows.length; i++) {
-         const cell = rows[i].getElementsByTagName('td')[1];
-         if (cell.innerHTML.trim() !== "") {
-             counterVt++;
-         }
-     }
- 
-     document.querySelector(".spisok-vt").innerHTML = `Вт (${counterVt})`;
-     localStorage.setItem('counterVt', counterVt);
- }
- 
- document.querySelector(".spisok-vt").innerHTML = `Вт (${counterVt})`;
- 
- // For Ср
- let counterSr = localStorage.getItem('counterSr') || 0;
- 
- function updateCounterSr(cellId) {
-     const table = document.querySelector(".dztable-sr");
-     const rows = table.getElementsByTagName('tr');
- 
-     counterSr = 0;
- 
-     for (let i = 1; i < rows.length; i++) {
-         const cell = rows[i].getElementsByTagName('td')[1];
-         if (cell.innerHTML.trim() !== "") {
-             counterSr++;
-         }
-     }
- 
-     document.querySelector(".spisok-sr").innerHTML = `Ср (${counterSr})`;
-     localStorage.setItem('counterSr', counterSr);
- }
- 
- document.querySelector(".spisok-sr").innerHTML = `Ср (${counterSr})`;
- 
- // For Чт
- let counterCht = localStorage.getItem('counterCht') || 0;
- 
- function updateCounterCht(cellId) {
-     const table = document.querySelector(".dztable-cht");
-     const rows = table.getElementsByTagName('tr');
- 
-     counterCht = 0;
- 
-     for (let i = 1; i < rows.length; i++) {
-         const cell = rows[i].getElementsByTagName('td')[1];
-         if (cell.innerHTML.trim() !== "") {
-             counterCht++;
-         }
-     }
- 
-     document.querySelector(".spisok-cht").innerHTML = `Чт (${counterCht})`;
-     localStorage.setItem('counterCht', counterCht);
- }
- 
- document.querySelector(".spisok-cht").innerHTML = `Чт (${counterCht})`;
- 
- // For Пт
- let counterPt = localStorage.getItem('counterPt') || 0;
- 
- function updateCounterPt(cellId) {
-     const table = document.querySelector(".dztable-pt");
-     const rows = table.getElementsByTagName('tr');
- 
-     counterPt = 0;
- 
-     for (let i = 1; i < rows.length; i++) {
-         const cell = rows[i].getElementsByTagName('td')[1];
-         if (cell.innerHTML.trim() !== "") {
-             counterPt++;
-         }
-     }
- 
-     document.querySelector(".spisok-pt").innerHTML = `Пт (${counterPt})`;
-     localStorage.setItem('counterPt', counterPt);
- }
- 
- document.querySelector(".spisok-pt").innerHTML = `Пт (${counterPt})`;
- 
- // For Сб
- let counterSb = localStorage.getItem('counterSb') || 0;
- 
- function updateCounterSb(cellId) {
-     const table = document.querySelector(".dztable-sb");
-     const rows = table.getElementsByTagName('tr');
- 
-     counterSb = 0;
- 
-     for (let i = 1; i < rows.length; i++) {
-         const cell = rows[i].getElementsByTagName('td')[1];
-         if (cell.innerHTML.trim() !== "") {
-             counterSb++;
-         }
-     }
- 
-     document.querySelector(".spisok-sb").innerHTML = `Сб (${counterSb})`;
-     localStorage.setItem('counterSb', counterSb);
- }
- 
- document.querySelector(".spisok-sb").innerHTML = `Сб (${counterSb})`;
- 
+    function update() {
+        const table = document.querySelector(`.dztable-${day}`);
+        const rows = table.getElementsByTagName('tr');
+
+        count = Array.from(rows).slice(1).reduce((acc, row) => {
+            const cell = row.getElementsByTagName('td')[1];
+            return acc + (cell.innerHTML.trim() !== "" ? 1 : 0);
+        }, 0);
+
+        document.querySelector(`.spisok-${day}`).innerHTML = `${day} (${count})`;
+        localStorage.setItem(counter, count);
+    }
+
+    update();
+
+    return update;
+}
+
+// Создаем функции обновления для каждого дня
+const updatePn = updateCounter("pn", "counterPn");
+const updateVt = updateCounter("vt", "counterVt");
+const updateSr = updateCounter("sr", "counterSr");
+const updateCht = updateCounter("cht", "counterCht");
+const updatePt = updateCounter("pt", "counterPt");
+const updateSb = updateCounter("sb", "counterSb");
+
+// Вызываем функции обновления при загрузке страницы
+document.addEventListener("DOMContentLoaded", function () {
+    updatePn();
+    updateVt();
+    updateSr();
+    updateCht();
+    updatePt();
+    updateSb();
+});
+
+// Удаление всех значений и обновление счетчиков
+document.getElementById("deleteAll").addEventListener("click", function () {
+    // Ваш код для удаления значений
+
+    // После удаления вызываем соответствующие функции обновления
+    updatePn();
+    updateVt();
+    updateSr();
+    updateCht();
+    updatePt();
+    updateSb();
+});
--- js/editor.js
+++ js/editor.js
@@ -1,46 +1,63 @@
 const editBtn = document.getElementById("editBtn");
-const editables = document.querySelectorAll("#pntime, #pn1, #pn2, #pn3, #pn4, #pn5, #pn6, #pn7, #pn8, #dzpn1, #dzpn2, #dzpn3, #dzpn4, #vttime, #vt1, #vt2, #vt3, #vt4, #vt5, #vt6, #vt7, #vt8, #dzvt1, #dzvt2, #dzvt3, #dzvt4, #srtime, #sr1, #sr2, #sr3, #sr4, #sr5, #sr6, #sr7, #sr8, #dzsr1, #dzsr2, #dzsr3, #dzsr4, #chttime, #cht1, #cht2, #cht3, #cht4, #cht5, #cht6, #cht7, #cht8, #dzcht1, #dzcht2, #dzcht3, #dzcht4, #pttime, #pt1, #pt2, #pt3, #pt4, #pt5, #pt6, #pt7, #pt8, #dzpt1, #dzpt2, #dzpt3, #dzpt4, #sbtime, #sb1, #sb2, #sb3, #sb4, #sb5, #sb6, #sb7, #sb8, #dzsb1, #dzsb2, #dzsb3, #dzsb4");
+  const editables = document.querySelectorAll("#pntime, #kpn1, #kpn2, #kpn3, #kpn4, #dzpn1, #dzpn2, #dzpn3, #dzpn4, #vttime, #kvt1, #kvt2, #kvt3, #kvt4, #dzvt1, #dzvt2, #dzvt3, #dzvt4, #srtime, #ksr1, #ksr2, #ksr3, #ksr4, #dzsr1, #dzsr2, #dzsr3, #dzsr4, #chttime, #kcht1, #kcht2, #kcht3, #kcht4, #dzcht1, #dzcht2, #dzcht3, #dzcht4, #pttime, #kpt1, #kpt2, #kpt3, #kpt4, #dzpt1, #dzpt2, #dzpt3, #dzpt4, #sbtime, #ksb1, #ksb2, #ksb3, #ksb4, #dzsb1, #dzsb2, #dzsb3, #dzsb4");
 
-if (typeof Storage !== "undefined") {
+  if (typeof Storage !== "undefined") {
     for (let i = 0; i < editables.length; i++) {
-        const localStorageItem = editables[i].getAttribute("id");
-        if (localStorage.getItem(localStorageItem) !== null) {
-            editables[i].innerHTML = localStorage.getItem(localStorageItem);
-        }
+      const localStorageItem = editables[i].getAttribute("id");
+      if (localStorage.getItem(localStorageItem) !== null) {
+        editables[i].innerHTML = localStorage.getItem(localStorageItem);
+      }
     }
-}
+  }
 
-editBtn.addEventListener("click", function () {
+  editBtn.addEventListener("click", function () {
     if (!editables[0].isContentEditable) {
-        for (let i = 0; i < editables.length; i++) {
-            editables[i].contentEditable = "true";
-        }
-        editBtn.innerHTML = "Сохр.";
-        editBtn.style.backgroundColor = "#6F9";
-        editBtn.style.padding = "3px";
+      for (let i = 0; i < editables.length; i++) {
+        editables[i].contentEditable = "true";
+      }
+      editBtn.innerHTML = "Сохр.";
+      editBtn.style.backgroundColor = "#6F9";
+      editBtn.style.padding = "3px";
     } else {
-        for (let i = 0; i < editables.length; i++) {
-            editables[i].contentEditable = "false";
-            localStorage.setItem(editables[i].getAttribute("id"), editables[i].innerHTML);
-        }
-        editBtn.innerHTML = "Отред.";
-        editBtn.style.backgroundColor = "#F96";
-        editBtn.style.padding = "3px";
-    }
-});
-
-document.addEventListener("keydown", function () {
-    for (let i = 0; i < editables.length; i++) {
+      for (let i = 0; i < editables.length; i++) {
+        editables[i].contentEditable = "false";
         localStorage.setItem(editables[i].getAttribute("id"), editables[i].innerHTML);
+      }
+      editBtn.innerHTML = "Отред.";
+      editBtn.style.backgroundColor = "#F96";
+      editBtn.style.padding = "3px";
     }
-});
+  });
 
-document.querySelector("#addh1").addEventListener("click", function () {
+  document.addEventListener("keydown", function () {
+    for (let i = 0; i < editables.length; i++) {
+      localStorage.setItem(editables[i].getAttribute("id"), editables[i].innerHTML);
+    }
+  });
+
+  document.querySelector("#deleteAll").addEventListener("click", function () {
+    const password = prompt("Введите пароль для удаления всех ДЗ:");
+  
+    // Проверка правильности пароля
+    if (password === "596") {
+      if (confirm("Вы уверены, что хотите удалить все ДЗ?")) {
+        for (let i = 0; i < editables.length; i++) {
+          editables[i].innerHTML = "";
+          localStorage.removeItem(editables[i].getAttribute("id"));
+        }
+        alert("Все значения удалены успешно.");
+      }
+    } else {
+      alert("Неправильный пароль. ДЗ не удалены.");
+    }
+  });
+
+  document.querySelector("#addh1").addEventListener("click", function () {
     const text = prompt("What text do you want the heading to have?", "Heading");
     editables[2].innerHTML += `<h1>${text}</h1>`;
-});
+  });
 
-document.querySelector("#addh2").addEventListener("click", function () {
+  document.querySelector("#addh2").addEventListener("click", function () {
     const text = prompt("What text do you want the heading to have?", "Heading");
     editables[2].innerHTML += `<h2>${text}</h2>`;
-});
+  });
